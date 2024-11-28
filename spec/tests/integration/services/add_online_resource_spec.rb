2f3f8ee21fb8046405eabfc48f1b7346a6486983
MING-CHUNLee <mindy80230@gmail.com>
2024-11-28 10:40:41 +0800

    fix:new testing

--- spec/tests/integration/services/add_online_resource_spec.rb
+++ spec/tests/integration/services/add_online_resource_spec.rb
@@ -20,24 +20,14 @@ describe 'RoutePlanner Service Integration Test' do
       DatabaseHelper.wipe_database
     end
 
-    it 'HAPPY: should be able to find and save remote project to database' do
-      # GIVEN: a valid request for an existing remote project:
-      videos = RoutePlanner::Youtube::VideoRecommendMapper.new(API_KEY).find(KEY_WORD)
-
-      # WHEN: the service is called with the request form object
-      videos_made = RoutePlanner::Service::AddOnlineResource.new.call(KEY_WORD)
+    it 'HAPPY: should be able to find and save remote online resource to database' do
+      resources_made = RoutePlanner::Service::AddOnlineResource.new.call(KEY_WORD)
       # THEN: the result should report success..
-      _(videos_made.success?).must_equal true
+      _(resources_made.success?).must_equal true
 
-      # ..and provide a video entity with the right details
-      rebuilt_videos = videos_made.value!
-      rebuilt_videos.zip(videos).each do |rebuilt, original|
-        _(rebuilt.original_id.class).must_equal(original.original_id.class)
-        _(rebuilt.topic.class).must_equal(original.topic.class)
-        _(rebuilt.url.class).must_equal(original.url.class)
-        _(rebuilt.platform.class).must_equal(original.platform.class)
-        _(rebuilt.for_skill.class).must_equal(original.for_skill.class)
-      end
+      # ..and provide a confirmation message
+      rebuilt_message = resources_made.value!
+      _(rebuilt_message).must_equal 'Online resource saved successfully.'
     end
   end
 end
