f4f5c19dfa3f991f7b149f97589bd6c9ca268066
Fifi <fifi.ding@iss.nthu.edu.tw>
2024-12-01 00:03:44 +0800

    join table

--- app/infrastructure/database/orm/map_orm.rb
+++ app/infrastructure/database/orm/map_orm.rb
@@ -6,9 +6,20 @@ module RoutePlanner
   module Database
     # Object Relational Mapper for Maps
     class MapOrm < Sequel::Model(:maps)
-      # one_to_many :routeplanners,
-      #             class: :'RoutePlanner::Database::RoutePlannerOrm'
+      one_to_many :map_skills,
+                  class: :'RoutePlanner::Database::MapSkillOrm',
+                  key: :map_id
+
+      many_to_many :skills,
+                   class: :'RoutePlanner::Database::SkillOrm',
+                   join_table: :map_skills,
+                   left_key: :map_id, right_key: :skill_id
+
       plugin :timestamps, update_on_create: true
+      
+      def self.db_find_or_create(map)
+        first(map_name: map[:map_name]) || create(map)
+      end
     end
   end
 end
